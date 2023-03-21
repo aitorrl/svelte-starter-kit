9f4362bcf1b68d387d359b8beff0ab6ffb57b62d
Aitor Resano <aitor.resano@biko2.com>
2023-03-21 14:28:33 +0100

    Layouts config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,23 @@ module.exports = {
         primary: ['"Cabin"'],
       },
     },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    screens: {
+      mobile: "767px",
+      // => @media (min-width: 767px) { ... }
+
+      tablet: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      desktop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktoplarge: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [require("daisyui")],
 };
